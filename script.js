e4e67f6925aa1fe54b356d6271cfc2de461b83bf
yacine <yacineaaddi@gmail.com>
2025-06-19 23:27:09 +0100

    Editing JS file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Selecting elements
+
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
@@ -16,6 +17,7 @@ const btnHold = document.querySelector('.btn--hold');
 let scores, currentScore, activePlayer, playing;
 
 // Starting conditions
+
 const init = function () {
   scores = [0, 0];
   currentScore = 0;
@@ -44,24 +46,31 @@ const switchPlayer = function () {
 };
 
 // Rolling dice functionality
+
 btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. Generating a random dice roll
+    
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     // 2. Display dice
+    
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
     // 3. Check for rolled 1
+    
     if (dice !== 1) {
       // Add dice to current score
+      
       currentScore += dice;
       document.getElementById(
         `current--${activePlayer}`
       ).textContent = currentScore;
     } else {
+      
       // Switch to next player
+      
       switchPlayer();
     }
   }
@@ -69,16 +78,22 @@ btnRoll.addEventListener('click', function () {
 
 btnHold.addEventListener('click', function () {
   if (playing) {
+    
     // 1. Add current score to active player's score
+    
     scores[activePlayer] += currentScore;
+    
     // scores[1] = scores[1] + currentScore
 
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
     // 2. Check if player's score is >= 100
+    
     if (scores[activePlayer] >= 100) {
+      
       // Finish the game
+      
       playing = false;
       diceEl.classList.add('hidden');
 
@@ -89,7 +104,9 @@ btnHold.addEventListener('click', function () {
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
     } else {
+      
       // Switch to the next player
+      
       switchPlayer();
     }
   }
