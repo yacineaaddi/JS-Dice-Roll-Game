d5ec72d64222e8baf611ee57dc3ce1c07fedf476
yacine <yacineaaddi@gmail.com>
2025-08-11 23:57:19 +0100

    Update CSS Comments

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ btnHold.addEventListener('click', function () {
 
     // 2. Check if player's score is >= 100
     
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 50) {
       
       // Finish the game
       
